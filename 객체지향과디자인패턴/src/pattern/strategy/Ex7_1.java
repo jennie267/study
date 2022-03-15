dcd4f4eb0139e858414a67af7a6121bbbcc0fe9f
jennie267 <jennieee267@gmail.com>
2022-03-15 23:40:23 +0900

    init

--- 객체지향과디자인패턴/src/pattern/strategy/Ex7_1.java
+++ 객체지향과디자인패턴/src/pattern/strategy/Ex7_1.java
@@ -1,5 +1,7 @@
 package pattern.strategy;
 
+import java.util.List;
+
 /**
  * 가격 계산 모듈에 할인 정책 구현이 포함된 코드
  */
@@ -16,6 +18,23 @@ public class Ex7_1 {
                     sum += item.getPrice();
             }
             return sum;
+        }
+    }
+
+    public class Item {
+        int price;
+        boolean isFresh;
+
+        public Item(int price, boolean isFresh) {
+            this.price = price;
+            this.isFresh = isFresh;
+        }
+
+        public int getPrice() {
+            return this.price;
+        }
+        public boolean isFresh() {
+            return this.isFresh;
         }
     }
 }
